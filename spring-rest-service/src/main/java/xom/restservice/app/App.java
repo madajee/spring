4f15ead632da1ece552482aa0a4d84aa8796ad33
madajee <MADAJEE@EMAIL.com>
2016-11-26 18:50:41 -0500

    spring-rest-service with SpringBoot

---
+++ spring-rest-service/src/main/java/xom/restservice/app/App.java
@@ -0,0 +1,16 @@
+package xom.restservice.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
