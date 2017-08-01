c6ed7e2045585b5f9c6e22c39aba05ec8d19d2de
Jeetan Madaan <home@Jeetans-MacBook-Pro.local>
2017-08-01 08:36:04 -0400

    second commit:XmlBasedSpringAppSeedProject:configured logging

--- XmlBasedSpringAppSeedProject/src/com/ms3/xmlspringapp/service/OrganizationService.java
+++ XmlBasedSpringAppSeedProject/src/com/ms3/xmlspringapp/service/OrganizationService.java
@@ -2,6 +2,8 @@ package com.ms3.xmlspringapp.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +14,14 @@ import com.ms3.xmlspringapp.domain.Organization;
 @Service
 public class OrganizationService {
 	
+	private static Logger LOGGER = LoggerFactory.getLogger(OrganizationService.class);
+	
 	@Autowired
 	private OrganizationDao organizationDao;
 	
 	public List<Organization> getOrgList() {
 		List<Organization> orgList = organizationDao.getAllOrganizations();
+		LOGGER.info("In OrganizationService Class");
 		return orgList;
 	}
 }
